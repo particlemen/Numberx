c2c3ac9e7894e0472ea1ceb97cd6ff343ca174b5
Jorge Galleguillos <maligno@MacBook-Air-de-Jorge.local>
2018-03-13 20:13:33 -0300

    anadi hasta divide

--- Numberx.py
+++ Numberx.py
@@ -2,5 +2,12 @@
 class Numberx:
     def __init__(self,value):
         self.value = value
-    def plus(self, n)
+    def plus(self, n):
         self.value = self.value + n
+    def minus(self,n):
+        self.value = self.value - n
+    def times(self,n):
+        self.value = self.value * n
+    def divide(self,n):
+        self.value = self.value / n
+    
